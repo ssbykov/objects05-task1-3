88edd85b21685a1010e2ba03e7d4d59e9e06e6bc
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-17 16:52:25 +0300

    поправил метод add

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -2,7 +2,11 @@ object WallService {
     private var posts = emptyArray<Post>()
     private var id = 0
     fun addPost(post: Post): Post {
-        posts += post.copy(id = ++id)
+        posts += post.copy(
+            id = ++id,
+            likes = post.likes.copy(),
+            views = post.views.copy()
+        )
         return posts.last()
     }
 
