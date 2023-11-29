b4987c5cc20bdeb621873e13846f320794810af7
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-29 18:35:05 +0300

    Revert "Revert "добавлено решение задач 1-3 к ДЗ 07_exceptions""
    This reverts commit b222bd0366ed20be9d4dd2e6de52347768895e3a.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>) {
-
-    val posts = WallService
-    posts.addPost(Post(text = "Post1"))
-    posts.addPost(Post(text = "Post2"))
-    println(posts.update(Post(id = 2, text = "Post3")))
+    println(Reason.SPAM)
+//    val posts = WallService
+//    posts.addPost(Post(text = "Post1"))
+//    posts.addPost(Post(text = "Post2"))
+//    println(posts.update(Post(id = 2, text = "Post3")))
 
 }
 
